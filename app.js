bbf6356955a7f9d51badf693a8501d8c51b76830
Seggy Umboh <seggy.umboh@gmail.com>
2012-12-21 15:48:42 -0800

    Refactored StreamFilter

--- app.js
+++ app.js
@@ -1,81 +1,10 @@
-var Buffer = require('buffer').Buffer,
-    http = require('http'),
+var http = require('http'),
     parse = require('url').parse,
     request = require('request'),
-    zlib = require('zlib');
+    StreamFilter = require('./lib/streamfilter').StreamFilter;
 
-var Stream = require('stream').Stream,
-    util = require('util');
-
-function StreamFilter(callback) {
-  var self = this;
-
-  Stream.call(this);
-
-  this.prologue = this.epilogue = function() {};
-  this.readable = true;
-  this.writable = true;
-
-  if (callback) {
-    self.prologue = function() {
-      self.dst.write(new Buffer(callback + '('));
-    };
-    self.epilogue = function() {
-      self.dst.write(new Buffer(')'));
-    };
-  }
-
-  self.on('pipe', function(src) {
-    self.src = src;
-    src.on('response', hookup);
-  });
-
-  function hookup(res) {
-    if (self.dst.setHeader) {
-      for (var i in res.headers) {
-        if (!~['content-length', 'content-type', 'content-encoding'].indexOf(i)) {
-          self.dst.setHeader(i, res.headers[i]);
-        }
-      }
-      self.dst.setHeader('content-type', 'text/javascript');
-    }
-    self.dst.statusCode = res.statusCode;
-    self.prologue();
-    if (~['gzip', 'deflate'].indexOf(res.headers['content-encoding'])) zipup();
-  }
-
-  function zipup() {
-    var finalDst = self.dst;
-    self.dst = zlib.createUnzip();
-    self.dst.on('data', function(chunk) {
-      finalDst.write(chunk);
-    });
-    self.dst.on('end', function() {
-      finalDst.write(new Buffer(')'));
-      finalDst.end();
-    });
-    self.epilogue = function() {};
-  }
-}
-util.inherits(StreamFilter, Stream);
-
-StreamFilter.prototype.pipe = function(dst) {
-  this.dst = dst;
-  return dst;
-}
-
-StreamFilter.prototype.write = function(chunk) {
-  this.dst.write(chunk);
-}
-
-StreamFilter.prototype.end = function(chunk) {
-  if (chunk) this.write(chunk);
-  this.epilogue();
-  this.dst.end();
-}
-
-http.createServer(function (req, res) {
-  var callback, target, rec;
+var app = module.exports = http.createServer(function(req, res) {
+  var callback, rec, target, url;
 
   url = parse(req.url, true);
   target = decodeURIComponent(url.pathname.substring(1));
@@ -96,4 +25,6 @@ http.createServer(function (req, res) {
   } else {
     rec.pipe(new StreamFilter(callback)).pipe(res);
   }
-}).listen(process.env.PORT || 3000, '127.0.0.1');
+});
+
+if ('undefined' === typeof beforeEach) app.listen(process.env.PORT || 3000);
