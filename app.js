cdb0ba755555da22e47658a2ca910adf2778bec8
Seggy Umboh <seggy.umboh@gmail.com>
2012-12-21 00:57:20 -0800

    Create a stream filter for padding the json

--- app.js
+++ app.js
@@ -1,9 +1,66 @@
-var http = require('http'),
+var Buffer = require('buffer').Buffer,
+    http = require('http'),
     parse = require('url').parse,
-    request = require('request');
+    request = require('request'),
+    zlib = require('zlib');
+
+var Stream = require('stream').Stream,
+    util = require('util');
+
+function StreamFilter(callback) {
+  var self = this;
+
+  Stream.call(this);
+
+  this.prologue = callback + '(';
+  this.epilogue = ')';
+  this.readable = true;
+  this.writable = true;
+
+  self.on('pipe', function(src) {
+    self.src = src;
+    src.on('response', hookup);
+  });
+
+  function hookup(res) {
+    if (self.dst.setHeader) {
+      for (var i in res.headers) {
+        if (!~['content-length', 'content-type'].indexOf(i)) {
+          self.dst.setHeader(i, res.headers[i]);
+        }
+      }
+      self.dst.setHeader('content-type', 'text/javascript');
+    }
+    self.dst.statusCode = res.statusCode;
+  }
+}
+util.inherits(StreamFilter, Stream);
+
+StreamFilter.prototype.pipe = function(dst) {
+  this.dst = dst;
+  return dst;
+}
+
+StreamFilter.prototype.write = function(chunk) {
+  if (this.prologue) {
+    var buf = new Buffer(this.prologue);
+    this.emit('data', buf);
+    this.dst.write(buf);
+    delete this.prologue;
+  }
+  this.emit('data', chunk);
+  this.dst.write(chunk);
+}
+
+StreamFilter.prototype.end = function(chunk) {
+  if (chunk) this.write(chunk);
+  if (this.epilogue) this.write(this.epilogue);
+  this.emit('end');
+  this.dst.end();
+}
 
 http.createServer(function (req, res) {
-  var callback, client, target;
+  var callback, target, rec;
 
   url = parse(req.url, true);
   target = decodeURIComponent(url.pathname.substring(1));
@@ -18,23 +75,10 @@ http.createServer(function (req, res) {
     callback = url.query.callback;
   }
 
-  client = req.pipe(request(target));
+  rec = req.pipe(request(target));
   if (!callback) {
-    client.pipe(res);
+    rec.pipe(res);
   } else {
-    res.setHeader('Content-Type', 'text/javascript');
-    client.once('data', function(chunk) {
-      res.write(callback + '(');
-      res.write(chunk);
-      client.on('data', function(chunk) {
-        res.write(chunk);
-      });
-    });
-    client.on('end', function() {
-      res.end(');');
-    });
-    client.on('error', function(exception) {
-      res.trigger('error', exception);
-    });
+    rec.pipe(new StreamFilter(callback)).pipe(res);
   }
 }).listen(process.env.PORT || 3000, '127.0.0.1');
